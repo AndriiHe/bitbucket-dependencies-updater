6dd02f5b64f674014e83efc4370555d5f7fc3bb3
Andrii Heiko <andrii.heiko@wise-engineering.com>
2023-03-30 10:54:05 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+const token = process.env.BITBUCKET_ACCESS_TOKEN;
+const workspace = process.env.REPOSITY_WORKSPACE;
+const repository = process.env.REPOSITY_NAME;
+
+// TODO: authenticate
+// TODO: create branch
+// TODO: create a commit
+// TODO: create a PR
